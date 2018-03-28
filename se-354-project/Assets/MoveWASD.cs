d89ddf2bb49d629832f44f7768e4e12c944a759a
egesenkul <egesenkul35@hotmail.com>
2018-03-28 11:17:07 +0300

    Player can move with WASD
    Level generate script edited. A tank created which is player and it can be move with WASD keys.

---
+++ se-354-project/Assets/MoveWASD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWASD : MonoBehaviour {
+	private int speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update() {
+
+		transform.Translate (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime*speed);
+
+	}
+}
